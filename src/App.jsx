66264a346f3ae35838e2d5e2c707a874ab687554
kellisha2 <kellisha.andrew2@gmail.com>
2025-01-27 07:18:16 -0500

    fetch api works

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,43 @@ import { useState , useEffect } from 'react'
 import FeaturedMusic from './components/FeaturedMusic'
 import MusicCategories from './components/musicCategories'
 import MyLibrary from './components/MyLibrary'
-import FeaturedMusic from './components/FeaturedMusic'
+import PlayMusic from './components/playMusic'
 
 
 
 function App() {
 
-  
+  const [music, setMusic] = useState(null)
+
+
+  const getMusic = async () => {
+  const url = 'https://spotify23.p.rapidapi.com/tracks/?ids=4WNcduiCmDNfmTEz7JvmLv';
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '403dec8659msh8c2be53cf81e567p187a54jsnd8f5d07148de',
+      'x-rapidapi-host': 'spotify23.p.rapidapi.com'
+    }
+  };
+
+  try  {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    setMusic(data)
+    console.log(setMusic(data));
+  } catch (error) {
+    console.error(error);
+  }}
+
+
+  useEffect (() =>{
+    getMusic()
+  }, {})
 
 
   return (
     <>
-      
+      <PlayMusic />
     </>
   )
 }
--- src/components/PlayMusic.jsx
+++ src/components/PlayMusic.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 
 function PlayMusic() {
   return (
-    <div>PlayMusic</div>
-  )
+    <div className="">
+        <img></img>
+        {/* <FontAwesomeIcon icon="fa-solid fa-music" />
+        <p></p>
+        {<FontAwesomeIcon icon="fa-solid fa-backward-fast" />}
+        {<FontAwesomeIcon icon="fa-solid fa-play" />}
+        <FontAwesomeIcon icon="fa-solid fa-pause" />
+        <FontAwesomeIcon icon="fa-solid fa-forward-fast" /> */}
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
+        )
 }
 
 export default PlayMusic
